e1c6d41087dec8f78e6eaa8aebdf38aac6165b07
n0an <nantog@gmail.com>
2019-05-29 16:27:08 +0300

    init

---
+++ DistractionJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DistractionJournal
+//
+//  Created by nag on 29/05/2019.
+//  Copyright © 2019 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
